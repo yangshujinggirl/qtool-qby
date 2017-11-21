80129c33a52f8d65203434b8918a771b23d2da05
李晨 <1366031854@qq.com>
2017-11-21 11:18:11 +0800

    搜索

--- qbys/src/constants/warehouseout/search.js
+++ qbys/src/constants/warehouseout/search.js
@@ -63,107 +63,85 @@ hinddataChange=(dates, dateStrings)=>{
 
   render() {
     const formItemLayout = {
-        labelCol: { span: 6 },
-        wrapperCol: { span: 18 },
+        // labelCol: { span: 6 },
+        // wrapperCol: { span: 18 },
       };
       const { getFieldDecorator } = this.props.form;
     return (
       <Form
         onSubmit={this.handleSearch}
       >
+
+
+
         <Row gutter={40} style={{position:'relative'}}>
             <Col span={22}>
                 <Row>
-                    <Col span={8}  style={{ display: 'block'}}>
+                    <div className='serach_form'>
                         <FormItem {...formItemLayout} label='门店名称'>
                             {getFieldDecorator('name')(
-                            <Input placeholder="请输入"/>
+                            <Input placeholder="请输入" className='form_input_width'/>
                             )}
                         </FormItem>
-                    </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='收货人电话'>
-            {getFieldDecorator('recTelephone')(
-              <Input placeholder="请输入" />
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='收货人'>
-            {getFieldDecorator('recName')(
-              <Input placeholder="请输入" />
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='配货单号'>
-            {getFieldDecorator('orderNo')(
-              <Input placeholder="请输入" />
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='商品条码'>
-            {getFieldDecorator('barcode')(
-              <Input placeholder="请输入" />
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='订单状态'>
-            {getFieldDecorator('status')(
-              <Select allowClear={true} placeholder="请选择">
-              <Option value='10'>待分配</Option>
-              <Option value='40'>待拣核</Option>
-              <Option value='80'>待发货</Option>
-              <Option value='90'>已发货</Option>
-            </Select>
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='打印状态'>
-            {getFieldDecorator('print')(
-                <Select allowClear={true} placeholder="请选择">
-                    <Option value='false'>未打印</Option>
-                    <Option value='true'>已打印</Option>
-                </Select>
-            )}
-          </FormItem>
-        </Col>
-        <Col span={8}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='订单类型'>
-            {getFieldDecorator('type')(
-              <Select allowClear={true} placeholder="请选择">
-                <Option value='10'>门店</Option>
-                <Option value='11'>直邮</Option>
-                <Option value='20'>采退</Option>
-            </Select>
-            )}
-          </FormItem>
-        </Col>
-
-
-
-        <Col span={12}  style={{ display: 'block'}}>
-          <FormItem {...formItemLayout} label='合单时间'>
-            {getFieldDecorator('time')(
-              <RangePicker
-              showTime
-              format="YYYY-MM-DD HH:mm:ss"
-              style={{'width':'100%'}}
-              onChange={this.hinddataChange.bind(this)}
-            />
-            )}
-          </FormItem>
-        </Col>
-
-        
+                        <FormItem {...formItemLayout} label='收货人电话'>
+                            {getFieldDecorator('recTelephone')(
+                            <Input placeholder="请输入" />
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='收货人'>
+                            {getFieldDecorator('recName')(
+                            <Input placeholder="请输入" />
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='配货单号'>
+                            {getFieldDecorator('orderNo')(
+                            <Input placeholder="请输入" />
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='商品条码'>
+                            {getFieldDecorator('barcode')(
+                            <Input placeholder="请输入" />
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='订单状态'>
+                            {getFieldDecorator('status')(
+                            <Select allowClear={true} placeholder="请选择">
+                            <Option value='10'>待分配</Option>
+                            <Option value='40'>待拣核</Option>
+                            <Option value='80'>待发货</Option>
+                            <Option value='90'>已发货</Option>
+                            </Select>
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='打印状态'>
+                            {getFieldDecorator('print')(
+                                <Select allowClear={true} placeholder="请选择">
+                                    <Option value='false'>未打印</Option>
+                                    <Option value='true'>已打印</Option>
+                                </Select>
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='订单类型'>
+                            {getFieldDecorator('type')(
+                            <Select allowClear={true} placeholder="请选择">
+                                <Option value='10'>门店</Option>
+                                <Option value='11'>直邮</Option>
+                                <Option value='20'>采退</Option>
+                            </Select>
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label='合单时间'>
+                            {getFieldDecorator('time')(
+                            <RangePicker
+                            showTime
+                            format="YYYY-MM-DD HH:mm:ss"
+                            onChange={this.hinddataChange.bind(this)}
+                            />
+                            )}
+                        </FormItem>     
+                    </div>
                 </Row>
             </Col>
-
-
-
             <Col span={2} style={{'position':'absolute','right':'0','bottom':'20px',paddingLeft:'0'}}>
                 <Row type="flex" justify="space-between" align="bottom">
                     <Col span={8}>
@@ -173,15 +151,7 @@ hinddataChange=(dates, dateStrings)=>{
                     </Col>   
                 </Row>
             </Col>
-
-
-
-
-
-       
-        
         </Row>
-       
       </Form>
     );
   }
