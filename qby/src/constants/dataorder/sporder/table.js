44b9603227835fa651f2430ddc60c7d7bee8d619
李晨 <1366031854@qq.com>
2018-01-20 11:00:12 +0800

    da

--- qby/src/constants/dataorder/sporder/table.js
+++ qby/src/constants/dataorder/sporder/table.js
@@ -1,11 +1,12 @@
 import { connect } from 'dva';
 import EditableTable from '../../../components/table/tablebasic';
-import { DatePicker,Switch,Button } from 'antd'
+import { DatePicker,Switch,Button,Modal } from 'antd'
 import moment from 'moment';
 const { MonthPicker, RangePicker } = DatePicker;
 const dateFormat = 'YYYY-MM-DD';
 import {timeForMat} from '../../../utils/meth';
 import {GetServerData} from '../../../services/services';
+const confirm = Modal.confirm;
 
 class DataclassTable extends React.Component {
 	constructor(props) {
@@ -44,7 +45,43 @@ class DataclassTable extends React.Component {
             endDate:null
         }
 	}
+    
+    //导出数据
+	exportData = (type,data) => {
+		const values={
+			type:type,
+			downloadParam:data,
+		}
+		const result=GetServerData('qerp.web.sys.doc.task',values);
+		result.then((res) => {
+			return res;
+		}).then((json) => {
+			if(json.code=='0'){
+				var _dispatch=this.props.dispatch
+				confirm({
+					title: '数据已经进入导出队列',
+					content: '请前往下载中心查看导出进度',
+					cancelText:'稍后去',
+					okText:'去看看',
+					onOk() {
+						const paneitem={title:'下载中心',key:'000001',componkey:'000001',data:null}
+						_dispatch({
+							type:'tab/firstAddTab',
+							payload:paneitem
+						});
+						_dispatch({
+							type:'downlaod/fetch',
+							payload:{code:'qerp.web.sys.doc.list',values:{limit:15,currentPage:0}}
+						});
+					},
+					onCancel() {
+						
+					},
+	  			});
+			}
+		})
 	
+	}
 	
 	
 	//列表数据请求   
@@ -68,6 +105,7 @@ class DataclassTable extends React.Component {
 	render() {
         const startDate=timeForMat(7).t2
         const endDate=timeForMat(7).t1
+        const values={startDate:this.state.startDate,endDate:this.state.endDate}
 		return (
             <div>
                 <p>订单变化数据</p>
@@ -77,7 +115,11 @@ class DataclassTable extends React.Component {
                         format={dateFormat}
                         onChange={this.hindChange.bind(this)}
                     />
-                    <Button type="primary" className='ml10'>导出数据</Button>
+                    <Button 
+                        type="primary" 
+                        className='ml10'
+                        onClick={this.exportData.bind(this,40,values)}
+                    >导出数据</Button>
                 </div>
                 <div className='mt10'>
                 <EditableTable
