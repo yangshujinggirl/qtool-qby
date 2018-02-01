6b1acf0563dd505d7b4a68349196e8b7fdb91edd
李晨 <1366031854@qq.com>
2018-02-01 19:25:30 +0800

    ercrge

--- qby/src/echarts/classdes.js
+++ qby/src/echarts/classdes.js
@@ -185,7 +185,7 @@ class EchartsTest extends Component {
                     allowClear={false}
                 />
                 </div>
-                <div style={{position:"absolute",right:"100px",top:"40px",zIndex:'1000'}}><Clisklist listClick1={this.checkonChange1.bind(this)} listClick2={this.checkonChange2.bind(this)}/></div>
+                <div style={{position:"absolute",right:"100px",top:"40px",zIndex:'1000'}}><Clisklist listClick1={this.checkonChange1.bind(this)} listClick2={this.checkonChange2.bind(this)} type={this.state.type}/></div>
                 <div id="mainsclass" style={{ height: 400 }}></div>
             </div>
         );
