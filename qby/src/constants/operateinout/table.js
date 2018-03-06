2b53d3ac9c6aa5e0a76f49249d330980f1d22780
李晨 <1366031854@qq.com>
2018-03-06 15:34:40 +0800

    收支管理接口请求

--- qby/src/constants/operateinout/table.js
+++ qby/src/constants/operateinout/table.js
@@ -50,7 +50,7 @@ class OperateinoutTable extends React.Component {
             })
         }
         if(record.type=='25'){
-            const paneitem={title:'收银对账',key:'402000edit'+record.outId+'infomoney',data:{spShopId:record.spShopId,type:record.type,dayTime:record.dayTime,spMoneyDetailId:record.spMoneyDetailId},componkey:'402000infoMoney'}
+            const paneitem={title:'收银对账',key:'402000edit'+record.spMoneyDetailId+'infomoney',data:{spShopId:record.spShopId,type:record.type,dayTime:record.dayTime,spMoneyDetailId:record.spMoneyDetailId},componkey:'402000infoMoney'}
             this.props.dispatch({
                 type:'tab/firstAddTab',
                 payload:paneitem
