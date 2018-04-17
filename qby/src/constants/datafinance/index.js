3fdd25a52db5a6a1a9777a766fd6ea781cbba726
李晨 <1366031854@qq.com>
2018-04-17 10:34:35 +0800

    数据中心权限处理

--- qby/src/constants/datafinance/index.js
+++ qby/src/constants/datafinance/index.js
@@ -1,9 +1,9 @@
 
+import '../../style/dataManage.css';
 import {GetServerData} from '../../services/services';
 import { Button, Icon,Tabs,Input,Form,AutoComplete} from 'antd';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import '../../style/dataManage.css';
 
 import MdCostIndex from './mdCost/index';
 import CostCheckIndex from './costCheck/index';
@@ -17,7 +17,7 @@ class DataFinanceIndexForm extends React.Component{
 	state = {
         key:"1",
     };
-
+    //tab点击
     tabChange = (index)=>{
         this.setState({
             key:index
@@ -25,26 +25,94 @@ class DataFinanceIndexForm extends React.Component{
     }
 
   	render(){
+        const rolelists=this.props.data.rolelists
+		//门店成本
+		const spchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.tasks"
+        })
+        //成本核算
+        const suanchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //采购到货
+        const cgchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //门店发票
+        const piaorole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
         const { getFieldDecorator } = this.props.form;       
      	return(
         	<div className='content_box stock-tabs'>
                 <Tabs defaultActiveKey="1"  onTabClick={this.tabChange.bind(this)}>
-                    <TabPane tab="门店成本" key="1">
-                        {this.state.key == 1 && <MdCostIndex/>} 
-                    </TabPane>
-                    <TabPane tab="成本核算" key="2">
-                        {this.state.key == 2 && <CostCheckIndex/>} 
-                    </TabPane>
-                    <TabPane tab="采购到货" key="3">
-                        {this.state.key == 3 && <CgArrivalIndex/>} 
-                    </TabPane>
-                    <TabPane tab="门店发票" key="4">
-                        {this.state.key == 4 && <MdInvoiceIndex/>} 
-                    </TabPane>
+                    {
+                        spchengrole?
+                        <TabPane tab="门店成本" key="1">
+                            {this.state.key == 1 && <MdCostIndex/>} 
+                        </TabPane>
+                        :null
+                    }
+                    {
+                        suanchengrole?
+                        <TabPane tab="成本核算" key="2">
+                            {this.state.key == 2 && <CostCheckIndex/>} 
+                        </TabPane>
+                        :null
+                    }
+                    {
+                        cgchengrole?
+                        <TabPane tab="采购到货" key="3">
+                            {this.state.key == 3 && <CgArrivalIndex/>} 
+                        </TabPane>
+                        :null
+                    }
+                    {
+                        piaorole?
+                        <TabPane tab="门店发票" key="4">
+                            {this.state.key == 4 && <MdInvoiceIndex/>} 
+                        </TabPane>
+                        :null
+                    }
                 </Tabs>
         	</div>
       	)
-  	}
+    }
+    componentDidMount(){
+        const rolelists=this.props.data.rolelists
+		//门店成本
+		const spchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.tasks"
+        })
+        //成本核算
+        const suanchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //采购到货
+        const cgchengrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //门店发票
+        const piaorole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        if(spchengrole){
+            this.tabChange('1')
+        }else{
+            if(suanchengrole){
+                this.tabChange('2')
+            }else{
+                if(cgchengrole){
+                    this.tabChange('3')
+                }else{
+                    if(piaorole){
+                        this.tabChange('4')
+                    }   
+                }
+            }
+        }
+    }
+      
 }
 
 const DataFinanceIndex = Form.create()(DataFinanceIndexForm);
--- qby/src/constants/dataws/index.js
+++ qby/src/constants/dataws/index.js
@@ -20,23 +20,86 @@ class DatawsIndex extends React.Component{
             key:index
         })
     }
-  	render(){       
+  	render(){ 
+        const rolelists=this.props.data.rolelists
+		//实时库存
+		const shicunrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.tasks"
+        })
+        //历史库存
+        const hiscunrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //商品效期
+        const goodqirole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })      
      	return(
         	<div className='content_box stock-tabs'>
                 <Tabs defaultActiveKey="1" onTabClick={this.tabChange.bind(this)}>
-                    <TabPane tab="实时库存" key="1">
-                        {this.state.key == '1' && <DatawsonIndex/>} 
-                    </TabPane>
-                    <TabPane tab="历史库存" key="2">
-                        {this.state.key == '2' && <DatawshisIndex/>} 
-                    </TabPane>
-                    <TabPane tab="商品效期" key="3">
-                        {this.state.key == '3' && <DatawstimeIndex/>} 
-                    </TabPane>
+                    {
+                        shicunrole?
+                        <TabPane tab="实时库存" key="1">
+                            {this.state.key == '1' && <DatawsonIndex/>} 
+                        </TabPane>
+                        :null
+                    }
+                    {
+                        hiscunrole?
+                        <TabPane tab="历史库存" key="2">
+                            {this.state.key == '2' && <DatawshisIndex/>} 
+                        </TabPane>
+                        :null
+                    }
+                    {
+                        goodqirole?
+                        <TabPane tab="商品效期" key="3">
+                            {this.state.key == '3' && <DatawstimeIndex/>} 
+                        </TabPane>
+                        :null
+
+                    }
+                    
+                    
+                    
                 </Tabs>
         	</div>
       	)
-  	}
+    }
+    componentDidMount(){
+        const rolelists=this.props.data.rolelists
+		//实时库存
+		const shicunrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.tasks"
+        })
+        //历史库存
+        const hiscunrole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        //商品效期
+        const goodqirole=rolelists.find((currentValue,index)=>{
+			return currentValue.remark=="qerp.web.sys.doc.task"
+        })
+        if(shicunrole){
+            this.setState({
+                key:'1'
+            })
+        }else{
+            if(hiscunrole){
+                this.setState({
+                    key:'2'
+                })
+            }else{
+                if(goodqirole){
+                    this.setState({
+                        key:'3'
+                    })
+                }
+            }
+        }
+
+    }
+      
 }
 
 export default connect()(DatawsIndex);
