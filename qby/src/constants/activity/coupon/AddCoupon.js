3cd0100e1a79972a0827455f7bb8f41f6222020a
zhouhongye <zhy@qtoolsbaby.com>
2018-08-07 19:05:53 +0800

    删除

---
+++ qby/src/constants/activity/coupon/AddCoupon.js
@@ -0,0 +1,184 @@
+import React,{ Component } from 'react';
+import { Form, Select, Input, Button , message, Row, Col,DatePicker,Radio} from 'antd';
+import { connect } from 'dva'
+import { addCouponApi } from '../../../services/activity/coupon'
+import './index.css'
+const FormItem = Form.Item;
+const Option = Select.Option;
+const TextArea = Input.TextArea;
+const RadioGroup = Radio.Group;
+class AddCoupon extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      couponValidDay:true,
+      couponValidDate:false,
+    }
+  }
+
+  //保存
+  handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+      if(!err){
+        addCouponApi(values)
+        .then(res => {
+          if(res.code=='0'){
+            this.props.dispatch({
+    					type:'coupon/fetchList',
+    					payload:{}
+    				})
+            this.props.dispatch({
+    						type:'tab/initDeletestate',
+    						payload:this.props.componkey
+    				});
+            message.success('创建成功');
+          }
+        },err=>{
+          message.error('请求失败')
+        })
+      }
+    });
+  }
+  //单选框选择
+  choice =(e)=> {
+    const value = e.target.value;
+    if(value==1){
+      this.setState({couponValidDay:true,couponValidDate:false})
+    }else if(value==2){
+      this.setState({couponValidDay:false,couponValidDate:true})
+    };
+    this.props.form.resetFields(['couponValidDay','couponValidDate']);
+  }
+  //取消
+  cancel =()=> {
+    this.props.dispatch({
+        type:'tab/initDeletestate',
+        payload:this.props.componkey
+    });
+  }
+  render(){
+    const { getFieldDecorator } = this.props.form;
+    const { cBanner } = this.props;
+    const { couponValidDay,couponValidDate} = this.state;
+    return(
+      <div className='addCoupon'>
+        	<Form className="addUser-form operatebanner-form">
+            <FormItem
+              label="代金券名称"
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+              {getFieldDecorator('couponName', {
+                  rules: [{ required: true, message: '请输入代金券名称'}],
+                })(
+                  <Input placeholder="请输入10字以内代金券名称" maxLength='10' autoComplete="off"/>
+              )}
+            </FormItem>
+            <FormItem
+              label="代金券场景"
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+            {getFieldDecorator('couponUseScene', {
+              rules: [{ required: true, message: '请选择代金券场景' }],
+            })(
+              <Select placeholder="请选择banner状态">
+                  <Option value="1">新用户专享券</Option>
+                  <Option value="0">注券</Option>
+              </Select>
+            )}
+            </FormItem>
+            <Row>
+              <Col className='radio'>
+                <FormItem
+                  label="券有效期"
+                  labelCol={{ span: 3,offset: 1 }}
+                  wrapperCol={{ span: 8 }}
+                >
+                  {getFieldDecorator('couponValid',{
+                      rules: [{ required: true, message: '请选择券有效期' }],
+                      onChange:this.choice
+                  })(
+                    <RadioGroup >
+                        <Radio value="1">用户领取时间起</Radio>
+                        <Radio value="2">特定时间到</Radio>
+                    </RadioGroup>
+                  )}
+                </FormItem>
+              </Col>
+              <Col className='limitDay'>
+                <FormItem>
+                  {getFieldDecorator('couponValidDay',{
+                    rules: [{ required:couponValidDay, message: '请填写用户领取时间' }],
+                  })(
+                    <Input disabled = {!couponValidDay} />
+                  )}
+                </FormItem>
+                <FormItem>
+                   {getFieldDecorator('couponValidDate',{
+                       rules: [{ required:couponValidDate , message: '请填写特定时间' }],
+                    })(
+                      <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" disabled = {!couponValidDate} />
+                   )}
+                </FormItem>
+              </Col>
+            </Row>
+            <FormItem
+              label="代金券金额"
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+            {getFieldDecorator('couponMoney', {
+              rules: [{required: true, message: '请输入代金券金额'},{pattern:/^(([1-9][0-9]*)|(([0]\.\d{0,2}|[1-9][0-9]*\.\d{0,2})))$/,message: '请输入最多2位小数正数'}],
+            })(
+              <div><Input style={{width:'80%'}} placeholder = '请输入代金券金额'/>　元</div>
+            )}
+            </FormItem>
+            <FormItem
+              label='使用门槛'
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+              {getFieldDecorator('couponFullAmount', {
+                rules: [{required: true, message: '请输入代金券金额'},{pattern:/^[^[+]{0,1}(\d+)$/,message: '请输入正整数'}],
+              })(
+                <div><span>满　</span><Input style={{width:'50%'}} />　元可用</div>
+              )}
+            </FormItem>
+            <FormItem
+              label='代金券数'
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+            {getFieldDecorator('couponCount', {
+              rules: [{required: true, message: '请输入代金券金额'},{pattern:/^10000$|^(\d|[1-9]\d)$/,message: '0-10000之间的正整数'}],
+            })(
+              <div><Input placeholder='请输入小于10000的正整数' style={{width:'80%'}}/>　张</div>
+            )}
+            </FormItem>
+            <FormItem
+              label='代金券备注'
+              labelCol={{ span: 3,offset: 1 }}
+              wrapperCol={{ span: 6 }}
+            >
+            {getFieldDecorator('couponRemark', {
+            })(
+                <TextArea placeholder='请输入300字以下代金券备注' maxLength='300' rows={6} />
+            )}
+            </FormItem>
+          	<FormItem wrapperCol={{ offset: 3}}>
+            		<Button style={{marginRight:'100px'}} onClick={this.cancel}>取消</Button>
+            		<Button type="primary" onClick={this.handleSubmit}>保存</Button>
+          	</FormItem>
+        	</Form>
+      </div>
+    )
+  }
+}
+const AddCoupons = Form.create()(AddCoupon);
+function mapStateToProps(state){
+  const { coupon } = state;
+  return coupon
+}
+export default connect(mapStateToProps)(AddCoupons);
---
+++ qby/src/constants/cooperate/marketResource/AddStaff.js
@@ -0,0 +1,367 @@
+import React, { Component} from 'react'
+import { Form, Select, Input, Button, message, Upload, Icon} from 'antd'
+import {connect} from 'dva'
+import { resourceDetailApi, addStaffApi  } from '../../../services/cooperate/marketResource'
+// import UpLoadImg from '../../../components/UploadImg/index.js';
+import AddImgText from '../../../components/AddTextImg/index.js';
+import './index.less'
+const FormItem = Form.Item;
+const TextArea = Input.TextArea;
+const Option = Select.Option;
+const formItemLayout = {
+  labelCol: {span:3},
+  wrapperCol: {span:6}
+}
+
+class AddStaff extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      DescArr:[],
+      fields:null,
+      listDate:null
+    }
+  }
+  //确认添加
+  save =()=> {
+    this.props.form.validateFieldsAndScroll((err,values) => {
+      var marketResCp = [];
+      const { DescArr } = this.state;
+      DescArr.map( (item,index)=> {
+        let obj = {};
+        obj.content = item.type;
+        obj.marketRestCpId = item.content;
+        marketResCp.push(obj);
+      });
+      //判断是修改还是新增
+      var values_
+      if(this.props.data){
+        const marketResId = this.props.data.marketResId
+        values_ = {marketRes:{marketResCp,...values,marketResId:marketResId}}
+      }else{
+        values_ ={marketRes:{marketResCp,...values}}
+      };
+      if(!err){
+        this.submit(values_)
+      };
+    })
+  }
+  submit(values){
+    addStaffApi(values)
+    .then(res=>{
+      if(res.code == '0'){
+        message.success('成功');
+        this.props.dispatch({
+            type:'marketResource/fetchList',
+            payload:{}
+        });
+        this.props.dispatch({
+						type:'tab/initDeletestate',
+						payload:this.props.componkey
+				});
+      };
+    },err=>{
+      message.error('失败')
+    })
+  }
+  //取消
+  cancel =()=> {
+    this.props.dispatch({
+				type:'tab/initDeletestate',
+				payload:this.props.componkey
+		});
+  }
+  //初始化
+  componentDidMount(){
+    this.setState({DescArr:[]});
+    if(this.props.data){
+      const marketResId = {marketResId:this.props.data.marketResId};
+      resourceDetailApi(marketResId)
+      .then(res => {
+        if(res.code=='0'){
+          this.setState({
+            listDate:res.marketRes,
+            DescArr:JSON.parse(res.marketRes.content)
+          });
+        };
+      },err=>{
+      })
+    }
+  }
+
+  //添加文本/图片
+  addContent =(type)=>{
+    let { DescArr } = this.state;
+    if(type=='text'){
+      DescArr.push({type:1,content:''})
+    }else{
+      DescArr.push({type:2,content:''})
+    }
+    this.setState({DescArr});
+  }
+//图片更改-----》更改state的值
+  changeState =(DescArr)=> {
+    this.setState({DescArr})
+  }
+  render(){
+    const fileDomain=eval(sessionStorage.getItem('fileDomain'));
+    const { getFieldDecorator } = this.props.form;
+    const { listDate, DescArr }= this.state;
+    if(listDate){
+      for(var key in listDate){
+        if(listDate['marketTypeId']==1){
+          listDate['marketTypeStr'] = '供应商'
+        }else if(listDate['marketTypeId']==2){
+          listDate['marketTypeStr'] = '媒体'
+        }else if(listDate['marketTypeId']==3){
+          listDate['marketTypeStr'] = '品牌商'
+        }else if(listDate['marketTypeId']==4){
+          listDate['marketTypeStr'] = 'KOI'
+        }else if(listDate['marketTypeId']==5){
+          listDate['marketTypeStr'] = '其他'
+        }
+      }
+    }
+    const uploadButton = (
+      <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    return(
+      <div className='addStaff'>
+        <Form>
+          <FormItem
+            {...formItemLayout}
+            label='联系人'
+          >
+            {
+              getFieldDecorator('name', {
+                rules: [{ required: true, message: '请输入联系人'}],
+                initialValue:listDate?listDate.name:null
+              })(
+                <Input
+                  placeholder='请输入16字以下的联系人'
+                  maxLength='16'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='联系电话'
+          >
+            {
+              getFieldDecorator('mobile', {
+                rules: [{ required: true, message: '请输入联系电话'}],
+                initialValue:listDate?listDate.mobile:null
+              })(
+                <Input placeholder='请输入16字以下的联系电话' maxLength='16' autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='资源类型'
+          >
+            {
+              getFieldDecorator('marketTypeId', {
+                rules: [{ required: true, message: '请选择资源类型'}],
+                initialValue:listDate?listDate.marketTypeStr:null
+              })(
+                <Select placeholder="请选择资源类型">
+                  <Option value="1">供应商</Option>
+                  <Option value="2">媒体</Option>
+                  <Option value="3">品牌商</Option>
+                  <Option value="4">KOl</Option>
+                  <Option value="5">其他</Option>
+                </Select>
+              )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='联系微信'
+          >
+            {
+              getFieldDecorator('wechat', {
+                initialValue:listDate?listDate.wechat:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的联系微信'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='联系邮箱'
+          >
+            {
+              getFieldDecorator('email', {
+                initialValue:listDate?listDate.email:null
+              })(
+                <Input
+                  placeholder='请输入64字以下的联系邮箱'
+                  maxLength='64'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='公司全称'
+          >
+            {
+              getFieldDecorator('company', {
+                initialValue:listDate?listDate.company:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的公司名称'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='公司简称'
+          >
+            {
+              getFieldDecorator('companyShort', {
+                initialValue:listDate?listDate.companyShort:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的公司简称'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='所在部门'
+          >
+            {
+              getFieldDecorator('department', {
+                initialValue:listDate?listDate.department:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的所在部门'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='所在职位'
+          >
+            {
+              getFieldDecorator('job', {
+                initialValue:listDate?listDate.job:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的所在职位'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='开户银行'
+          >
+            {
+              getFieldDecorator('bank', {
+                initialValue:listDate?listDate.bank:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的银行名称'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='银行卡号'
+          >
+            {
+              getFieldDecorator('bankNo', {
+                initialValue:listDate?listDate.bankNo:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的银行卡号'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='开户名'
+          >
+            {
+              getFieldDecorator('bankName', {
+                initialValue:listDate?listDate.bankName:null
+              })(
+                <Input
+                  placeholder='请输入32字以下的开户名'
+                  maxLength='32'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='子女情况'
+          >
+            {
+              getFieldDecorator('family', {
+                initialValue:listDate?listDate.family:null
+              })(
+                <TextArea
+                  rows={5}
+                  placeholder='请输入100字以下的子女情况'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label='其他备注'
+          >
+            {
+              getFieldDecorator('remark', {
+                initialValue:listDate?listDate.remark:null
+              })(
+                <TextArea
+                  rows={6}
+                  placeholder='请输入200字以下的其他备注'
+                  autoComplete="off"/>
+              )
+            }
+          </FormItem>
+          <AddImgText
+            DescArr = {DescArr}
+            addContent = {this.addContent}
+            changeState = {this.changeState}
+           />
+         <FormItem wrapperCol={{ offset: 3}} style={{marginTop:'30px'}}>
+            <Button style={{marginRight:'60px'}} onClick={this.cancel}>取消</Button>
+            <Button type="primary" onClick={this.save}>确定</Button>
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const AddStaffs = Form.create()(AddStaff);
+function mapStateToProps(state){
+  const { marketResource } = state;
+  return { marketResource }
+}
+
+export default connect(mapStateToProps)(AddStaffs)
