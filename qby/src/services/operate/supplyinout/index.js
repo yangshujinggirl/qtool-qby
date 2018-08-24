c05f097c7f39ab1cfb9e1933dfe594f30f59cbc6
zhouhongye <zhy@qtoolsbaby.com>
2018-08-24 15:32:28 +0800

    结算状态接口

--- qby/src/services/operate/supplyinout/index.js
+++ qby/src/services/operate/supplyinout/index.js
@@ -1,10 +1,18 @@
 import ajax from '../../../utils/req.js'
 
-//发票管理 拿取数据
+//供应商收支列表
 export function getListApi(values){
   values = JSON.stringify(values)
   return ajax.post('/webrest.htm',{
       code:'qerp.web.pd.settle.query',
+      data:values
+  })
+}
+//结算状态更改
+export function changeStatusApi(values){
+  values = JSON.stringify(values)
+  return ajax.post('/webrest.htm',{
+      code:'qerp.web.pd.settle.update',
       data:values
   })
 }
