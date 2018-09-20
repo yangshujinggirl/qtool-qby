066c3b9e2ab765861607784a2ed7f5748a0fc67c
zhouhongye <zhy@qtoolsbaby.com>
2018-09-20 13:41:12 +0800

    商品中心基础商品空值问题

--- qby/src/constants/goods/baseGoods/AddGoods.js
+++ qby/src/constants/goods/baseGoods/AddGoods.js
@@ -265,6 +265,20 @@ class AddGoodsForm extends Component {
     const { pdSpuId, source } =this.props.data;
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
+      for(var key in values){
+        if(String(values[key])==''){ //spu商品属性为'',置为null
+          values[key] = null;
+        };
+        if(key == 'pdSkus' && values[key]){ //有sku,sku商品属性值为'',置为null
+          values[key].map((item)=>{
+            for(var childKey in item){
+              if(item[childKey]==''){
+                item[childKey] = null;
+              };
+            };
+          });
+        };
+      };
       console.log(values)
       if (!err) {
         if(pdSpuId) {
