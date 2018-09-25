b32ef8cbd903a72a16b2986da4553cf0ce8192c6
zhouhongye <zhy@qtoolsbaby.com>
2018-09-25 18:07:46 +0800

    operate

---
+++ qby/src/constants/operate/supplyinout/BillDetails.js
@@ -0,0 +1,201 @@
+	import React from 'react';
+import EditableTable from '../../../components/table/tablebasic';
+import { Button, Form, Input, message,Card, Modal, DatePicker } from 'antd';
+import { connect } from 'dva';
+import { billDetailsApi,changeAccountApi } from '../../../services/operate/supplyinout'
+import UpLoadImg from '../../../components/UploadImg/index.js';
+import './index.less'
+import moment from 'moment'
+const FormItem = Form.Item;
+const { TextArea } = Input;
+const RangePicker = DatePicker.RangePicker
+
+class BillDetail extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state={
+      visible:false,
+      pdSettles:{},
+      logs:[]
+		}
+		this.columns = [{
+  			title: '操作',
+  			dataIndex: 'operateName',
+        key:'1',
+		  }, {
+  			title: '操作时间',
+  			dataIndex: 'operateTime',
+        key:'2'
+  		}, {
+  			title: '操作人',
+  			dataIndex: 'operateUser',
+        key:'3',
+  		},{
+  			title: '备注',
+  			dataIndex: 'remark',
+        key:'4'
+  		}];
+}
+componentDidMount(){
+	this.initData()
+}
+initData =()=> {
+	const {pdSettlementId} = this.props.data;
+	billDetailsApi({pdSettlementId})
+	.then(res=>{
+		if(res.code == '0'){
+			this.setState({
+				pdSettles:res.pdSettles,
+				logs:res.logs
+			});
+		};
+	})
+}
+//点击修改结算到期日
+changeStatus =()=> {
+	this.setState({visible:true})
+}
+//确定
+onOk =()=> {
+	this.props.form.validateFieldsAndScroll((err,values) => {
+		const {pdSettlementId} = this.props.data;
+		values.expireDate = moment(values.expireDate).format('YYYY-MM-DD');
+		values.pdSettlementId = pdSettlementId;
+		if(!err){
+			changeAccountApi(values)
+			.then(res => {
+				if(res.code == '0'){
+					this.setState({visible:false});
+					this.props.form.resetFields(['expireDate','remark']);
+					this.initData()
+				};
+			})
+		}
+	});
+}
+//取消
+onCancel =()=> {
+	this.setState({
+		visible:false
+	});
+	this.props.form.resetFields(['expireDate','remark'])
+}
+
+render(){
+	console.log(this.props);
+  const {
+		payType,
+    settlementNo,
+    outNo,
+    typeStr,
+    statusStr,
+    expireDate,
+    name,
+    userName,
+    amountSum,
+		createTime
+  } = this.state.pdSettles;
+  const logs = this.state.logs;
+	const { getFieldDecorator }= this.props.form;
+	const rolelists = this.props.data.rolelists;
+	//修改结算到期日
+	const accountChange = rolelists.find((currentValue,index)=>{
+		return currentValue.url=="qerp.web.pd.settle.update"
+	})
+	return(
+			<div className='bill_detail'>
+        <div className='mb10 change_day'>
+					{
+						this.props.data.status == 0 && accountChange
+						?
+							<Button
+								size='large'
+								onClick={this.changeStatus}
+								type='primary'>修改结算到期日
+							</Button>
+						:	null
+					}
+
+        </div>
+        <div className='mb10'>
+          <Card title='工单信息'>
+            <div className='cardlist'>
+                <div className='cardlist_item'><label>结算单号：</label><span>{settlementNo}</span></div>
+                <div className='cardlist_item'><label>关联订单：</label><span>{outNo}</span></div>
+                <div className='cardlist_item'><label>结算单生成时间：</label><span>{createTime}</span></div>
+                <div className='cardlist_item'><label>类型：</label><span>{typeStr}</span></div>
+                <div className='cardlist_item'><label>是否已结算：</label><span>{statusStr}</span></div>
+                <div className='cardlist_item'><label>结算到期日：</label><span>{expireDate}</span></div>
+                <div className='cardlist_item'><label>供应商名称：</label><span>{name}</span></div>
+                <div className='cardlist_item'><label>关联业务人员：</label><span>{userName}</span></div>
+                <div className='cardlist_item'>
+									{ payType == 10
+										? <span><label>应收金额：</label><span>-{amountSum}</span></span>
+										:<span><label>应付金额：</label><span>+{amountSum}</span></span>
+									}
+								</div>
+            </div>
+          </Card>
+        </div>
+        <div className='mb20 server-bill-edit'>
+          <EditableTable
+            columns={this.columns}
+            title='处理日志'
+            bordered={true}
+            dataSource = {logs}
+          />
+        </div>
+				<Modal
+					visible={this.state.visible}
+					closable = { true }
+					onOk={this.onOk}
+          onCancel={this.onCancel}
+					wrapClassName='billModal'
+				>
+					<Form>
+						<FormItem
+							label="当前结算到期日为"
+							labelCol={{ span: 6 }}
+							wrapperCol={{ span: 12 }}
+						>
+							<span>{expireDate}</span>
+						</FormItem>
+						<FormItem
+							label="修改为"
+							labelCol={{ span: 6 }}
+							wrapperCol={{ span: 12 }}
+						>
+							{getFieldDecorator('expireDate',{
+								rules: [{required: true, message: '请选择结算到期日'}],
+							})(
+								<DatePicker
+									placeholder='请选择结算到期日'
+									className='data_size'
+									showTime
+								/>
+							)}
+						</FormItem>
+						<FormItem
+							label='修改原因'
+							labelCol={{ span: 6 }}
+							wrapperCol={{ span: 12 }}
+						>
+								{getFieldDecorator('remark',{
+									rules: [{required: true, message: '请输入修改原因'}],
+								})(
+									<TextArea placeholder='请输入修改原因' maxLength='50'/>
+								)}
+						</FormItem>
+					</Form>
+				</Modal>
+			</div>
+		)
+	}
+
+}
+const BillDetails = Form.create()(BillDetail);
+function mapStateToProps(state){
+  const { supplyinout } = state;
+  return { supplyinout }
+}
+export default connect(mapStateToProps)(BillDetails);
---
+++ qby/src/constants/operate/withdraw/index.js
@@ -0,0 +1,227 @@
+import React, { Component } from 'react';
+import { Button, Modal, message,Form,Select,Input } from 'antd'
+import { connect } from 'dva'
+import Columns from './columns/index'
+import Qtable from '../../../components/Qtable/index'; //表单
+import Qpagination from '../../../components/Qpagination/index'; //分页
+import FilterForm from './FilterForm/index'
+import { checkApi } from '../../../services/operate/withdraw'
+import moment from 'moment';
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class Withdraw extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      checkStatus:false,
+      shopName:'',
+      amount:'',
+      spCarryCashId:'',
+      field:{
+        shopName:'',
+        status:'',
+        payStatus:'',
+        carryCashNo:'',
+        dateStart:'',
+        dateEnd:''
+      },
+    }
+  }
+  //初始化数据
+  componentWillMount(){
+    this.initData()
+  }
+  //初始数据
+  initData =()=> {
+    this.props.dispatch({
+      type:'withdraw/fetchList',
+      payload:{}
+    })
+  }
+  //搜索框数据发生变化
+  searchDataChange =(values)=> {
+    const {rangePicker,..._values} = values;
+    if(rangePicker&&rangePicker[0]){
+      _values.dateStart =  moment(new Date(rangePicker[0]._d).getTime()).format('YYYY-MM-DD HH:mm:ss');;
+      _values.dateEnd = moment(new Date(rangePicker[1]._d).getTime()).format('YYYY-MM-DD HH:mm:ss');;
+    }
+    this.setState({field:_values});
+  }
+  //点击搜索
+  searchData = (values)=> {
+    this.props.dispatch({
+      type:'withdraw/fetchList',
+      payload:values
+    })
+  }
+  //点击分页
+  changePage =(current,limit)=> {
+    const currentPage = current-1;
+    const values = {...this.state.field,currentPage,limit}
+    this.props.dispatch({
+      type:'withdraw/fetchList',
+      payload:values
+    });
+  }
+  //pageSize改变时的回调
+  onShowSizeChange =({currentPage,limit})=> {
+    this.props.dispatch({
+      type:'withdraw/fetchList',
+      payload:{currentPage,limit}
+    });
+  }
+  //处理表格的点击事件
+  handleOperateClick(record,type){
+    switch(type) {
+      case "detail":
+        this.getDetail(record);
+        break;
+      case "edit":
+        this.getEdit(record);
+        break;
+    }
+  }
+  getDetail =(record)=> {
+    const paneitem = {
+      title:'提现详情',
+      key:`${this.props.componkey}edit${record.spCarryCashId}info`,
+      componkey:`${this.props.componkey}info`,
+      data:{
+        spCarryCashId:record.spCarryCashId,
+      }
+    };
+    this.props.dispatch({
+      type:'tab/firstAddTab',
+      payload:paneitem
+    });
+  }
+  getEdit =(record)=> {
+      this.setState({
+        visible:true,
+        shopName:record.shopName,
+        amount:record.amount,
+        spCarryCashId:record.spCarryCashId
+      })
+  }
+  notCheck =()=> {
+    this.setState({
+      checkStatus:true
+    },function(){
+      this.props.form.validateFieldsAndScroll((err,values) => {
+        if(!err){ //审核通过1 审核不通过2
+          values.status = 2;
+          values.spCarryCashId = this.state.spCarryCashId;
+          checkApi({spCarryCashs:values})
+          .then(res => {
+            if(res.code == "0"){
+              this.setState({
+                visible:false,
+              });
+              this.initData();
+            };
+          })
+          this.props.form.resetFields(['remark']);
+        };
+      });
+    });
+  }
+  check =()=> {
+    this.setState({
+      checkStatus:false
+    },()=>{
+      this.props.form.validateFieldsAndScroll((err,values) => {
+        if(!err){ //审核通过1 审核不通过2
+          values.status = 1;
+          values.spCarryCashId = this.state.spCarryCashId;
+          checkApi({spCarryCashs:values})
+          .then(res => {
+            if(res.code == "0"){
+              this.setState({
+                visible:false,
+              });
+              this.initData();
+            }
+          });
+          this.props.form.resetFields(['remark']);
+        };
+      });
+    });
+  }
+  onCancel =()=> {
+    this.setState({
+      visible:false
+    });
+    this.props.form.resetFields(['remark']);
+  }
+  render(){
+    const {dataList} = this.props.withdraw;
+    const { getFieldDecorator }= this.props.form;
+    const { shopName,amount,checkStatus} = this.state
+    return(
+      <div className='qtools-components-pages'>
+        <FilterForm
+          submit={this.searchData}
+          onValuesChange = {this.searchDataChange}
+        />
+        <div className="mt15">
+          <Qtable
+            dataSource = {dataList}
+            columns = {Columns}
+            onOperateClick = {this.handleOperateClick.bind(this)}
+          />
+        </div>
+        <Qpagination
+          data={this.props.withdraw}
+          onChange={this.changePage}
+          onShowSizeChange = {this.onShowSizeChange}
+        />
+        <Modal
+          visible={this.state.visible}
+          closable = { true }
+          onCancel={this.onCancel}
+          wrapClassName='withdraw'
+          footer={null}
+        >
+          <Form>
+            <FormItem
+              label="提现门店"
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 12 }}
+            >
+              <span>{shopName}</span>
+            </FormItem>
+            <FormItem
+              label="提现金额"
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 12 }}
+            >
+              <span>{amount}</span>
+            </FormItem>
+            <FormItem
+              label='不通过理由'
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 12 }}
+            >
+              {getFieldDecorator('remark',{
+                rules:[{required:checkStatus,message:'请输入不通过理由'}]
+              })(
+                <Input placeholder='请输入不通过的理由' maxLength='50' autoComplete="off"/>
+              )}
+            </FormItem>
+          </Form>
+          <div className='btnbox'>
+            <Button size='large' onClick={this.notCheck} className='check'>审核不通过</Button>
+            <Button type="primary" onClick={this.check} size='large'>审核通过</Button>
+          </div>
+        </Modal>
+      </div>
+    )
+  }
+}
+const Withdraws = Form.create()(Withdraw)
+function mapStateToProps(state){
+  const {withdraw} = state;
+  return {withdraw};
+}
+export default connect(mapStateToProps)(Withdraws);
