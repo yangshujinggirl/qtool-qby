8cfcf909274ff17fef3365ccf547a1c18c2458bd
zhouhongye <zhy@qtoolsbaby.com>
2018-10-08 10:51:09 +0800

    时间转换时如果当前月份是10不用用加0

--- qby/src/utils/meth.js
+++ qby/src/utils/meth.js
@@ -70,13 +70,13 @@ const confirm = Modal.confirm;
 		let time1 = new Date()
 	   	time1.setTime(time1.getTime()) //今天日期
 	   	let Y1 = time1.getFullYear()
-		let M1 = ((time1.getMonth() + 1) > 10 ? (time1.getMonth() + 1) : '0' + (time1.getMonth() + 1))
+		let M1 = ((time1.getMonth() + 1) >= 10 ? (time1.getMonth() + 1) : '0' + (time1.getMonth() + 1))
 		let D1 = (time1.getDate() >= 10 ? time1.getDate() : '0' + time1.getDate())
 		let timer1 = Y1 + '-' + M1 + '-' + D1 // 当前时间
 		let time2 = new Date()
 		time2.setTime(time2.getTime() - (24 * 60 * 60 * 1000 * (count-1)))
 		let Y2 = time2.getFullYear()
-		let M2 = ((time2.getMonth() + 1) > 10 ? (time2.getMonth() + 1) : '0' + (time2.getMonth() + 1))
+		let M2 = ((time2.getMonth() + 1) >= 10 ? (time2.getMonth() + 1) : '0' + (time2.getMonth() + 1))
 		let D2 = (time2.getDate() >= 10 ? time2.getDate() : '0' + time2.getDate())
 		let timer2 = Y2 + '-' + M2 + '-' + D2 // 之前的7天或者30天
 		return {
@@ -86,19 +86,19 @@ const confirm = Modal.confirm;
 	}
 
 
-	
+
 	export function timeForMats (count) {
 		// 拼接时间
 		let time1 = new Date()
 		time1.setTime(time1.getTime()) //当前日期
 	   	let Y1 = time1.getFullYear()  //当前年
-		let M1 = ((time1.getMonth() + 1) > 10 ? (time1.getMonth() + 1) : '0' + (time1.getMonth() + 1)) //当前月
+		let M1 = ((time1.getMonth() + 1) >= 10 ? (time1.getMonth() + 1) : '0' + (time1.getMonth() + 1)) //当前月
 		let D1 = (time1.getDate() >= 10 ? time1.getDate() : '0' + time1.getDate())  //当前天
 		let timer1 = Y1 + '-' + M1 + '-' + D1 + ' 23:59:59' // 当前时间
 		let time2 = new Date()
 		time2.setTime(time2.getTime() - (24 * 60 * 60 * 1000 * (count-1)))
 		let Y2 = time2.getFullYear()
-		let M2 = ((time2.getMonth() + 1) > 10 ? (time2.getMonth() + 1) : '0' + (time2.getMonth() + 1))
+		let M2 = ((time2.getMonth() + 1) >= 10 ? (time2.getMonth() + 1) : '0' + (time2.getMonth() + 1))
 		let D2 = (time2.getDate() >= 10 ? time2.getDate() : '0' + time2.getDate())
 		let timer2 = Y2 + '-' + M2 + '-' + D2 + ' 00:00:00' // 之前的7天或者30天
 		return {
@@ -121,7 +121,7 @@ const confirm = Modal.confirm;
 			Y2=12
 		}
 		let M1 = (Y2> 10 ? Y2 : '0' + Y2) //当前月
-		let timer1 = Y1 + '-' + M1 
+		let timer1 = Y1 + '-' + M1
 
 		return {
 		 	t1: timer1
