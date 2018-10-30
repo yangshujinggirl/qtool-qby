ad3ed6c334737aec99da3f3b4719ba52ccd45ea0
yangshujing <1009838410@qq.com>
2018-10-30 15:22:19 +0800

    商品

--- qby/src/constants/goods/cTipGoods/components/GoodsInfo/index.js
+++ qby/src/constants/goods/cTipGoods/components/GoodsInfo/index.js
@@ -159,7 +159,10 @@ class GoodsInfo extends Component {
   }
   renderGoldPrice =(text, record, index)=> {
     const { pdSpu } = this.props.cTipAddGoods;
-    let name = pdSpu.isSkus?`pdSkus[${index}].goldCardPrice`:'goldCardPrice'
+    let name = pdSpu.isSkus?`pdSkus[${index}].goldCardPrice`:'goldCardPrice';
+    if(!record.toCPrice) {
+      return ''
+    }
     return  <FormItem className='purchasePrice-input'>
               {this.props.form.getFieldDecorator(name,{
                 rules:[
@@ -177,6 +180,9 @@ class GoodsInfo extends Component {
   renderSilverPrice =(text, record, index)=> {
     const { pdSpu } = this.props.cTipAddGoods;
     let name = pdSpu.isSkus?`pdSkus[${index}].silverCardPrice`:'silverCardPrice';
+    if(!record.toCPrice) {
+      return ''
+    }
     return  <FormItem className='purchasePrice-input'>
               {this.props.form.getFieldDecorator(name,{
                 rules:[
