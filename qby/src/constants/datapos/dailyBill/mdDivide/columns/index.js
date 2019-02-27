c0fe0726546131f639620b8dcd72788be49e2365
zhy <17701799531@163.com>
2019-02-27 19:22:17 +0800

    分成对账

--- qby/src/constants/datapos/dailyBill/mdDivide/columns/index.js
+++ qby/src/constants/datapos/dailyBill/mdDivide/columns/index.js
@@ -1,59 +1,17 @@
-const platformMap={
-  '1':'Qtools App ios端',
-  '2':'Qtools App  安卓端',
-  '3':'小程序',
-}
-const deliveryMap={
-  '1':'门店自提',
-  '2':'同城配送',
-  '3':'快递邮寄',
-}
-
 const Columns = [{
      title: '订单号',
      dataIndex: 'orderNo',
-     render:(text, record)=>{
-       return(
-         <div>
-           <a href="javascript:;" className="theme-color" onClick={record.onOperateClick.bind(this)}>{text}</a>
-         </div>
-       )
-     }
    },{
-     title: '门店名称',
-     dataIndex: 'spShopName'
+     title: '业务类型',
+     dataIndex: 'orderTypeStr'
    },{
-     title: '下单平台',
-     dataIndex: 'platform',
-     render:(text,record)=> {
-       return platformMap[record.platform]
-     }
+     title: '订单分类',
+     dataIndex: 'shareTypeStr',
    },{
-     title: '配送方式',
-     dataIndex: 'delivery',
-     render:(text,record)=> {
-       return deliveryMap[record.deliveryType]
-     }
+     title: '分成金额',
+     dataIndex: 'shareProfitAmount',
    },{
-     title: '用户昵称',
-     dataIndex: 'nickName'
-   }, {
-     title: '用户电话',
-     dataIndex: 'mobilePhone'
-   },{
-     title: '商品数量',
-     dataIndex: 'qtySum'
-   },{
-     title: '订单金额',
-     dataIndex: 'payAmount'
-   },{
-     title: '优惠金额',
-     dataIndex: 'discountAmount'
-   },{
-     title: '流程状态',
-     dataIndex: 'orderStatusStr'
-   },{
-     title: '订单时间',
+     title: '订单完成时间',
      dataIndex: 'createTime'
-   }];
+   },];
    export  default Columns
