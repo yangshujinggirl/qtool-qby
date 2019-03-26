ba13ab668ace119e99b6d19b15ad0a1d17cdb17d
zhy <17701799531@163.com>
2019-03-26 21:52:14 +0800

    优惠券详情

--- qby/src/constants/activity/coupon/CouponDetail.js
+++ qby/src/constants/activity/coupon/CouponDetail.js
@@ -1,11 +1,28 @@
 import React,{ Component } from 'react';
+import {Card,Button,Table} from 'antd';
 import { couponInfoApi } from '../../../services/activity/coupon' //请求方法
-import './index.css'
+const columns = [
+  {
+    title:'操作',
+    dataIndex:'operation',
+  },{
+    title:'操作描述',
+    dataIndex:'operationMsg',
+  }, {
+    title:'操作时间',
+    dataIndex:'createTime',
+  },{
+    title:'操作人',
+    dataIndex:'operator',
+  },
+]
 class CouponDetail extends Component{
   constructor(props){
     super(props);
     this.state = {
-      couponInfo:{}
+      coupon:{},
+      operateLogs:[],
+      activityShop:[],
     }
   }
 
@@ -15,41 +32,57 @@ class CouponDetail extends Component{
     couponInfoApi({couponId:couponId})
     .then(res => {
       if(res.code=="0"){
-        this.setState({couponInfo:res.couponInfo})
+        this.setState({
+          coupon:res.coupon||[],
+          operateLogs:res.operateLogs||[],
+          activityShop:res.activityShop,
+        })
       }
     })
   }
 
   render(){
-    const { couponName,
-      couponUseSceneStr,
-      couponValidDay,
-      couponValidDate,
-      couponMoney,
-      couponFullAmount,
-      couponCount,
-      couponRemark,
-      couponUseScopeStr,
-      couponExplain
-     } = this.state.couponInfo
+    const {coupon,operateLogs,activityShop} = this.state;
     return(
-      <div className='couponDetail'>
-        <p className='tail'>优惠券名称：　{couponName}</p>
-        <p className='tail'>优惠券场景：　{couponUseSceneStr}</p>
-        <p className='tail'>券有效期：　　{
-            couponValidDay
-            ?
-            <span>用户领取时间起　{couponValidDay}　天</span>
-            :
-            <span>特定时间到　{couponValidDate}　止</span>
-          }
-        </p>
-        <p className='tail'>优惠券金额：　{couponMoney}元</p>
-        <p className='tail'>使用门槛：　　满　{couponFullAmount}　可用</p>
-        <p className='tail'>优惠券数：　　{couponCount}　张</p>
-        <p className='tail'>使用商品范围：{couponUseScopeStr} </p>
-        <p className='tail'>优惠券说明：{couponExplain} </p>
-        <p className='tail'>优惠券备注：　{couponRemark}</p>
+      <div>
+        <div className='mb10'>
+          <Card title='基本信息'>
+            <div className='cardlist'>
+              <div className='cardlist_item'><label>优惠券批次号：</label><span>{coupon.recName}</span></div>
+              <div className='cardlist_item'><label>优惠券有效日期：自用户领取7日起</label><span>{coupon.recMobile}</span></div>
+              <div className='cardlist_item'><label>优惠券状态：</label><span>{coupon.recAddress}</span></div>
+              <div className='cardlist_item'><label>创建人：</label><span>{coupon.shunFengExpenses}</span></div>
+              <div className='cardlist_item'><label>创建时间：</label><span>{coupon.knightName}</span></div>
+              <div className='cardlist_item'><label>优惠券金额：</label><span>{coupon.knightMobile}</span></div>
+              <div className='cardlist_item'><label>使用门槛：</label><span>{coupon.predictDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>优惠券数：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>发放方式：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>使用限制：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>剩余数量预警数：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>预警邮箱：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>优惠券说明：</label><span>{coupon.actualDeliveryTime}</span></div>
+              <div className='cardlist_item'><label>优惠券备注：</label><span>{coupon.actualDeliveryTime}</span></div>
+            </div>
+          </Card>
+        </div>
+        <div  className='mb10'>
+          <Card title='活动商品'>
+            <div className='cardlist'>适用商品类型：指定品牌【嘉宝娜】、【ashai】</div>
+          </Card>
+        </div>
+        <div  className='mb10'>
+          <Card title='活动门店'>
+            <div className='cardlist'>适用商品类型：指定品牌【嘉宝娜】、【ashai】</div>
+          </Card>
+        </div>
+        <div className='mb20'>
+          <Table
+            bordered
+            title={()=>'处理日志'}
+            dataSource={operateLogs}
+            columns={columns}
+            pagination={false}/>
+        </div>
       </div>
     )
   }
