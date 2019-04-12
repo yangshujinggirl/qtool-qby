0c09e25c006382fba5a1ea670ddce028f66e6cb8
zhy <17701799531@163.com>
2019-04-12 10:50:54 +0800

    b端直降

--- qby/src/constants/operate/bDown/addActivity.js
+++ qby/src/constants/operate/bDown/addActivity.js
@@ -45,7 +45,7 @@ class Addactivity extends Component {
         const {time,..._values} = values
         if(time && time[0]){
           _values.beginTime = moment(time[0]).format('YYYY-MM-DD hh:mm:ss');
-          _values.endTime = moment(time[0]).format('YYYY-MM-DD hh:mm:ss');
+          _values.endTime = moment(time[1]).format('YYYY-MM-DD hh:mm:ss');
         };
         _values.productList = this.state.goodList;
         _values.type = 2;
