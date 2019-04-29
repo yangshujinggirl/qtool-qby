23f6b6b689a0aea50625461d9fb212d8d0b177e9
zhy <17701799531@163.com>
2019-04-29 15:10:04 +0800

    用户订单-->保税订单详情增加虚拟订单号

--- qby/src/constants/online/orderuser/info.js
+++ qby/src/constants/online/orderuser/info.js
@@ -230,8 +230,13 @@ class OrderuserInfo extends React.Component{
 					this.setState({
 						ecOrderId:orderInfos.ecOrderId
 					});
-				}
+				};
 				let orderinfo = [];
+				if(this.props.data.record.channel == 2){
+					if(orderInfos.yzMerOrderNo.slice(0,2) == 'YH'){
+						orderinfo[1] = {lable:'虚拟单号',text:orderInfos.yzMerOrderNo}
+					};
+				};
 				if( this.props.data.record.channel == 1 ){ //有赞订单
 					orderinfo=[
 						{lable:'订单号',text:orderInfos.orderNo},
