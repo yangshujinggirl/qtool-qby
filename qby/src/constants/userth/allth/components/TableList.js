e9a22077462081d7d03fba01fd04efbd64c13097
zhy <17701799531@163.com>
2019-04-29 17:14:30 +0800

    用户退单

--- qby/src/constants/userth/allth/components/TableList.js
+++ qby/src/constants/userth/allth/components/TableList.js
@@ -69,33 +69,10 @@ class TableList extends Component {
 			  title: '退款金额',
 				dataIndex:'applyReturnQuota',
 				render: (text, record, index) => {
-					if(!this.props.returnType && record.applyReturnCount){ //如果是售中直接计算
 						return (
 							<Input value={text} disabled />
 						)
-					}else{ //售后
-						const { getFieldDecorator } = this.props.form;
-						const handleReturnQuota =(rule,value,callback)=> {
-							const {orderQuota,returnQuota} = record;
-							if (value && value >(Number(orderQuota)-Number(returnQuota)).toFixed(2)) {
-								 callback('不可超可退金额')
-							};
-								 callback();
-						};
-						return(
-							<FormItem className='applyReturnCount'>
-								{
-									getFieldDecorator(`applyReturnQuota`+index, {
-			 	 					 rules: [
-			 							 { pattern:/^\d+(\.\d{0,2})?$/,message:'小于等于两位小数的数字'},
-									 	 { validator: handleReturnQuota }
-			 						 ],
-			 	 				 })(<Input onBlur={this.getReturnQuota.bind(this,index)} disabled={record.buyCount == record.returnCount} autoComplete='off'/>)
-								}
-							</FormItem>
-						)
-				};
-			},
+	       },
   		}, {
   			width:'60px',
   		  title: '',
@@ -116,19 +93,16 @@ class TableList extends Component {
   getReturnCount =(index,e)=> {
     const {productList,returnType} = this.props;
     const {value} = e.target; //退款数量
-    if(!returnType){ //如果是售中直接计算出退款金额--->不可编辑
-      let returnMoney;
-      let item = productList[index];
-      if(value == item.buyCount - item.returnCount){ //退款数量等于剩余可退 --->表示全退---->用实付-已退（保证两者相加等于全部）
-        returnMoney = Subtr(Number(item.orderQuota) , Number(item.returnQuota) );
-        item.applyReturnQuota = returnMoney;
-      }else{ //单价 * 数量
-        const tempValue = item.orderQuota/item.buyCount * Number(value);
-        console.log(tempValue);
-        returnMoney = _.floor(tempValue,2) ;
-        item.applyReturnQuota = returnMoney;
-      };
-    }
+    let returnMoney;
+    let item = productList[index];
+    if(value == item.buyCount - item.returnCount){ //退款数量等于剩余可退 --->表示全退---->用实付-已退（保证两者相加等于全部）
+      returnMoney = Subtr(Number(item.orderQuota) , Number(item.returnQuota) );
+      item.applyReturnQuota = returnMoney;
+    }else{ //单价 * 数量
+      const tempValue = item.orderQuota/item.buyCount * Number(value);
+      returnMoney = _.floor(tempValue,2) ;
+      item.applyReturnQuota = returnMoney;
+    };
     productList[index].applyReturnCount = Number(value);
     this.props.productListChange(productList);
   }
