8ef0bf738b9bb1e0fd6488fc507470aab6301a02
zhy <17701799531@163.com>
2019-07-09 16:41:18 +0800

    用户退单权限

--- qby/src/constants/userth/index.js
+++ qby/src/constants/userth/index.js
@@ -13,11 +13,19 @@ class Userth extends React.Component {
       <div className="content_box stock-tabs">
         <Tabs defaultActiveKey="1">
           {rolelists.map((item, index) => (
-            <TabPane tab={item.name} key={index+1}>
-              <AllthIndex
-                rolelists={item.children}
-                componkey={this.props.componkey}
-              />
+            <TabPane tab={item.name} key={index + 1}>
+              {item.urResourceId == 208100 && (
+                <AllthIndex
+                  rolelists={item.children}
+                  componkey={this.props.componkey}
+                />
+              )}
+              {item.urResourceId == 208200 && (
+                <ToAuditThIndex
+                  rolelists={item.children}
+                  componkey={this.props.componkey}
+                />
+              )}
             </TabPane>
           ))}
         </Tabs>
