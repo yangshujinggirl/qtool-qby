e487cfbcd858a900312fda741a41e60b42b9c56a
zhy <17701799531@163.com>
2019-07-24 18:59:33 +0800

    单行商品

--- qby/src/constants/cConfig/HomeConfiguration/GoodsSet/components/DragField/index.js
+++ qby/src/constants/cConfig/HomeConfiguration/GoodsSet/components/DragField/index.js
@@ -1,53 +1,60 @@
-import React, { Component } from 'react';
-import { Table, Button } from 'antd';
-import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
-import update from 'immutability-helper';
-import MyTagControlContext from '../../../components/MyTagControlContext';
-import './index.less';
-
+import React, { Component } from "react";
+import { Table, Button } from "antd";
+import { DragDropContext, DragSource, DropTarget } from "react-dnd";
+import update from "immutability-helper";
+import MyTagControlContext from "../../../components/MyTagControlContext";
+import "./index.less";
 
 let dragingIndex = -1;
 
 class BodyRow extends React.Component {
   render() {
-    const { isOver, connectDragSource, connectDropTarget, moveRow, ...restProps } = this.props;
+    const {
+      isOver,
+      connectDragSource,
+      connectDropTarget,
+      moveRow,
+      ...restProps
+    } = this.props;
 
-    const style = { ...restProps.style, cursor: 'move' };
+    const style = { ...restProps.style, cursor: "move" };
 
     let { className } = restProps;
     if (isOver) {
       if (restProps.index > dragingIndex) {
-        className += ' drop-over-downward';
+        className += " drop-over-downward";
       }
       if (restProps.index < dragingIndex) {
-        className += ' drop-over-upward';
+        className += " drop-over-upward";
       }
     }
     return connectDragSource(
-      connectDropTarget(<tr {...restProps} className={className} style={style} />),
+      connectDropTarget(
+        <tr {...restProps} className={className} style={style} />
+      )
     );
   }
 }
 
 const rowSource = {
   beginDrag(props) {
-    let dragingIndex = props['data-row-index'];
-    let dragingParent = props['data-row-parent'];
+    let dragingIndex = props["data-row-index"];
+    let dragingParent = props["data-row-parent"];
     return {
       index: dragingIndex,
-      parent:dragingParent
+      parent: dragingParent
     };
-  },
+  }
 };
 
 const rowTarget = {
   drop(props, monitor) {
     const dragIndex = monitor.getItem().index;
-    const hoverIndex = props['data-row-index'];
-    const hoverParent = props['data-row-parent'];
+    const hoverIndex = props["data-row-index"];
+    const hoverParent = props["data-row-parent"];
     const dragParent = monitor.getItem().parent;
     // Don't replace items with themselves
-    if (dragIndex === hoverIndex&&dragParent===hoverParent) {
+    if (dragIndex === hoverIndex && dragParent === hoverParent) {
       return;
     }
     // Time to actually perform the action
@@ -59,15 +66,15 @@ const rowTarget = {
     // to avoid expensive index searches.
     monitor.getItem().index = hoverIndex;
     monitor.getItem().parent = hoverParent;
-  },
+  }
 };
-let DragableBodyRow  = DropTarget('row', rowTarget, (connect, monitor) => ({
+let DragableBodyRow = DropTarget("row", rowTarget, (connect, monitor) => ({
   connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver(),
+  isOver: monitor.isOver()
 }))(BodyRow);
 
-DragableBodyRow = DragSource('row', rowSource, connect => ({
-  connectDragSource: connect.dragSource(),
+DragableBodyRow = DragSource("row", rowSource, connect => ({
+  connectDragSource: connect.dragSource()
 }))(DragableBodyRow);
 
 class Field extends Component {
@@ -76,24 +83,26 @@ class Field extends Component {
   }
   components = {
     body: {
-      row: DragableBodyRow,
-    },
+      row: DragableBodyRow
+    }
   };
   //绑定方法
   processData(data) {
-    if(!this.props.onOperateClick) {
+    if (!this.props.onOperateClick) {
       return data;
     }
-    data && data.map((item, i) => {
-        item.onOperateClick = (type) => { this.props.onOperateClick(item,type) };
-    })
+    data &&
+      data.map((item, i) => {
+        item.onOperateClick = type => {
+          this.props.onOperateClick(item, type);
+        };
+      });
     return data;
   }
   render() {
-    const { goods, columnsTwo, columnsOne,totalList } =this.props;
-    let listTwo = this.processData(goods.listTwo)
-    let listOne = this.processData(goods.listOne)
-    // let { listTwo, listOne }=goods;
+    const { goods, columnsTwo, columnsOne, totalList = [] } = this.props;
+    let listTwo = this.processData(goods.listTwo);
+    let listOne = this.processData(goods.listOne);
     return (
       <div className="drag-tables-component">
         <Table
@@ -103,27 +112,34 @@ class Field extends Component {
           dataSource={listOne}
           components={this.components}
           onRow={(record, index) => ({
-            'data-row-key':record.key,
-            'data-row-parent':'listOne',
-            'data-row-index':index,
-            moveRow: this.props.moveRow,
-          })}/>
+            "data-row-key": record.key,
+            "data-row-parent": "listOne",
+            "data-row-index": index,
+            moveRow: this.props.moveRow
+          })}
+        />
         <Table
           bordered
           pagination={false}
           columns={columnsTwo}
           dataSource={listTwo}
           components={this.components}
-          footer={()=>{
-            totalList&&totalList.length <= 100 &&
-            <Button type="default" onClick={this.props.handleAdd}>+新增</Button>
+          footer={() => {
+            return (
+              !!totalList.length <= 100 && (
+                <Button type="default" onClick={this.props.handleAdd}>
+                  +新增
+                </Button>
+              )
+            );
           }}
           onRow={(record, index) => ({
-            'data-row-key':record.key,
-            'data-row-index':index,
-            'data-row-parent':'listTwo',
-            moveRow: this.props.moveRow,
-          })}/>
+            "data-row-key": record.key,
+            "data-row-index": index,
+            "data-row-parent": "listTwo",
+            moveRow: this.props.moveRow
+          })}
+        />
       </div>
     );
   }
