f2c1f4bf9428e5b59736a699427e9ffaa2dcf8e8
zhy <17701799531@163.com>
2019-07-25 19:59:39 +0800

    权限修改

--- qby/src/constants/cConfig/HomeConfiguration/ConfigurationList/columns/index.js
+++ qby/src/constants/cConfig/HomeConfiguration/ConfigurationList/columns/index.js
@@ -62,7 +62,7 @@ const IndexColumns = function(Look, Edit) {
                 编辑
               </span>
             )}
-            {((record.status == 1 || record.status == 2) && Look) && (
+            {((record.status == 1 || record.status == 2) && Edit) && (
               <span
                 className="action-btn"
                 onClick={() => record.onOperateClick("ban")}
