91c796dc69abdab45c99f1bac3e891116dad2e64
杨静 <yj@qtoolsbaby.com>
2019-07-29 10:28:29 +0800

    首页

--- qby/src/constants/cConfig/HomeConfiguration/components/FrameModal/index.js
+++ qby/src/constants/cConfig/HomeConfiguration/components/FrameModal/index.js
@@ -1,41 +1,51 @@
-import { Modal, Button, InputNumber, Form, message } from 'antd';
+import { Modal, Button, InputNumber, Form, message, Select } from 'antd';
 import React, { Component } from 'react';
 import './index.less';
 
+const optionBannerData=[{
+    key:1,
+  },{
+    key:2,
+  },{
+    key:3,
+  },{
+    key:4,
+  },{
+    key:5,
+  }]
+const optionIconData=[{
+    key:1,
+  },{
+    key:2,
+  },{
+    key:3,
+  },{
+    key:4,
+  }]
+const { Option } = Select;
 class App extends React.Component {
   onOk=()=> {
     this.props.form.validateFields(['frameNum'],(err, values) => {
       const { frameNum } =values;
       let tx =this.props.modName=='icon'?'坑位':'帧位';
       if(frameNum == this.props.activiKey) {
-        message.error(`不可选择当前${tx}`)
+        message.error(`不可选择当前${tx}`,1)
         return;
       }
       this.props.onOk(frameNum);
     });
   }
-  changeNum=(value)=> {
-    let max;
-    if(this.props.modName=='icon') {
-      max = 4;
-    } else {
-      max = 5;
-    }
-    if(value > max) {
-      this.props.form.setFieldsValue({frameNum:max})
-    }
-  }
   render() {
     const { visible, confirmLoading, modName } = this.props;
     const { getFieldDecorator } =this.props.form;
-    let text, max, unit;
+    let text, optionData, unit;
     if(modName=='icon') {
       text='将当前icon调整至第';
-      max = 4;
+      optionData = optionIconData;
       unit="坑"
     } else {
       text='将当前banner调整至第';
-      max = 5;
+      optionData = optionBannerData;
       unit="帧"
     }
     return (
@@ -49,11 +59,16 @@ class App extends React.Component {
         <div>
           {text}
           <Form.Item className="frame-item">
-            {getFieldDecorator('frameNum', {
-              initialValue: 1,
-              onChange:this.changeNum
+            {getFieldDecorator('frameNum',{
+              initialValue:1
             })(
-              <InputNumber min={1} max={max}/>
+              <Select>
+                {
+                  optionData.map((el,index) =>(
+                    <Option key={el.key} value={el.key}>{el.key}</Option>
+                  ))
+                }
+              </Select>
             )}
           </Form.Item>
           {unit}
