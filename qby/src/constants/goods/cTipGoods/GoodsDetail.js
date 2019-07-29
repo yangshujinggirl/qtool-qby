37a2e170fa84552ece38c4d91882e17a9e6dbcdf
zhy <17701799531@163.com>
2019-07-29 14:44:07 +0800

    商品详情报错

--- qby/src/constants/goods/cTipGoods/GoodsDetail.js
+++ qby/src/constants/goods/cTipGoods/GoodsDetail.js
@@ -110,16 +110,6 @@ class GoodsDetail extends Component {
               ))
             }
           </FormItem>
-          <FormItem label="上线平台" {...formItemLayout}>
-            {
-              pdSpu.platform&& pdSpu.platform.map(item=>(
-                <div>
-                  {item==1 && <label>c端app</label>}
-                  {item==2 && <label>小程序</label>}
-                </div>
-              ))
-            }
-          </FormItem>
           <FormItem label="品牌直供" {...formItemLayout}>
             <label>{pdSpu.brandDirectMail?'是':'否'}</label>
           </FormItem>
