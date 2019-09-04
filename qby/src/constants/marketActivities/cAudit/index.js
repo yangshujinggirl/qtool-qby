6e5942229961b573a5f984ebb59888a4ba394703
zhy <17701799531@163.com>
2019-09-04 18:37:58 +0800

    审核的title

--- qby/src/constants/marketActivities/cAudit/index.js
+++ qby/src/constants/marketActivities/cAudit/index.js
@@ -49,7 +49,7 @@ class cAudit extends Component{
   }
   handleOperateClick=(record,type)=>{ //type：'edit':审核 'detail':查看
     const paneitem = {
-      title:type=='detail'?'审核详情':'活动审核',
+      title:type=='detail'?'审核详情':'审核',
       key:`${this.props.componkey}editInfo`+record.approvalId,
       componkey:`${this.props.componkey}editInfo`,
       data:{
