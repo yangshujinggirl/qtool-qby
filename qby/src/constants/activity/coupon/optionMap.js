e364bbe215205785788778bf94fe403bbb0ffef2
杨静 <yj@qtoolsbaby.com>
2019-09-08 15:58:43 +0800

    修改bug

--- qby/src/constants/activity/coupon/optionMap.js
+++ qby/src/constants/activity/coupon/optionMap.js
@@ -1,26 +1,26 @@
 const couponUsageOption=[
   {
-    key:'10',
+    key:10,
     value:'不可与单品直降同享'
   },
   {
-    key:'11',
+    key:11,
     value:'不可与单品多级满赠同享'
   },
   {
-    key:'20',
+    key:20,
     value:'不可与专区多级满元赠同享'
   },
   {
-    key:'21',
+    key:21,
     value:'不可与专区多级满件赠同享'
   },
   {
-    key:'22',
+    key:22,
     value:'不可与专区多级满元减赠同享'
   },
   {
-    key:'23',
+    key:23,
     value:'不可与专区满件减免商品同享'
   }]
 // <Checkbox value={10} key={10}>不可与单品直降同享</Checkbox>
